554dfec639b50463938cb9eda68145091863a806
Alex Hearn <alex.d.hearn@gmail.com>
2020-03-11 13:33:08 -0500

    Saving progress before sending laptop in for repair

---
+++ chap3/regex.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_NUM_STATES 100
+
+typedef struct State {
+
+} State;
+
+State **construct_nfa(char *regex) {
+    int nextIndex = 0;
+    State **states = malloc(MAX_NUM_STATES * sizeof(State));
+
+
+
+    return states;
+}
+
+int main() {
+
+}
